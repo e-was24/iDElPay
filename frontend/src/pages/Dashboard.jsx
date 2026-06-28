ca8c080668e52baf1346dabd75f1d420a42592dc
e-was24 <satriaelan5@gmail.com>
2026-06-28 20:58:44 +0700

    big update

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { supabase } from '../lib/supabaseClient';
-import './css/Dashboard.css';
+import './css/dashboard.css';
 
 export default function Dashboard() {
   const navigate = useNavigate();
